d0a17782f74e99ca07e651524b386e5feff4bc13
FlytoTheSpace <146943385+FlytoTheSpace@users.noreply.github.com>
2024-03-19 15:21:09 +0000

    improved JavaScript Code

--- screch.js
+++ screch.js
@@ -1,29 +1,28 @@
 
-var a=document.querySelector('body')
-var math = Math.random() * 10
-var d = (parseInt(math))
-document.querySelector('.red').onclick = function dhruv(){
-  a.style.backgroundColor='red';
+const body = document.getElementsByTagName('body')[0]
+const $ = (query)=>document.querySelector(query)
+/**
+ * @returns A Number in-between 0 and 255
+ */
+const random = ()=>{
+  return Math.floor(Math.random() * 255);
+}
+
+const setColor = (color)=>{
+  if ( color === 'random' ){
+    const red = random();
+    const green = random();
+    const blue = random();
+    const color = `rgb(${red}, ${green}, ${blue})`; // Construct the RGB color string
+    body.style.background = color;
+    return null
+  }
+
+  body.style.background = color
 }
 
 
-document.querySelector('.green').onclick = function dhruv() {
-  a.style.backgroundColor = 'lightGreen';
-}
-
-
-document.querySelector('.blue').onclick = function dhruv() {
-  a.style.backgroundColor = 'blue';
-}
-
-function mohit() {
-  return Math.floor(Math.random() * 256); // Generate random number between 0 and 255
-}
-
-document.querySelector('.r').onclick = function dhruv() {
-  var red = mohit();
-  var green = mohit();
-  var blue = mohit();
-  var color = `rgb(${red}, ${green}, ${blue})`; // Construct the RGB color string
-  document.querySelector('body').style.backgroundColor = color;
-}
+$('#red').onclick = () => setColor('red');
+$('#green').onclick = () => setColor('lightGreen');
+$('#blue').onclick = () => setColor('blue');
+$('#random').onclick = () => setColor('random')
